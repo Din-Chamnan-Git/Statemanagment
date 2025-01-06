82791bcaf662a07c8adafa8b9deacb0ed1ca3083
unknown <chamnan862004@gmail.com>
2025-01-06 22:07:56 +0700

    add Slidable

--- lib/Provider/todo_list_provider.dart
+++ lib/Provider/todo_list_provider.dart
@@ -28,31 +28,6 @@ class TodoListProvider extends ChangeNotifier {
       title: 'Read 10 pages of a book',
       isCompleted: false,
     ),
-    TodoModel(
-      id: '1',
-      title: 'Buy groceries',
-      isCompleted: false,
-    ),
-    TodoModel(
-      id: '2',
-      title: 'Complete Flutter project',
-      isCompleted: true,
-    ),
-    TodoModel(
-      id: '3',
-      title: 'Call the plumber',
-      isCompleted: false,
-    ),
-    TodoModel(
-      id: '4',
-      title: 'Workout for 30 minutes',
-      isCompleted: true,
-    ),
-    TodoModel(
-      id: '5',
-      title: 'Read 10 pages of a book',
-      isCompleted: false,
-    ),
   ];
 
   List<TodoModel> get todo => _todo;
--- lib/todoPage.dart
+++ lib/todoPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list_provider/Provider/todo_list_provider.dart';
 
@@ -23,49 +24,61 @@ class Todopage extends StatelessWidget {
 
                 return Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Card(
-                    color: Colors.deepPurple[400],
-                    child: ListTile(
-                      title: Text(
-                        todo.title,
-                        style: TextStyle(
-                          color: Colors.white, // Text color
-                          fontWeight: FontWeight.bold, // Bold text
-                          decoration: todo.isCompleted
-                              ? TextDecoration
-                                  .lineThrough // Apply line-through if completed
-                              : TextDecoration.none, // No decoration otherwise
-                          decorationColor: Colors.red, // Set the line color
-                          decorationThickness: 3.0, // Set the line thickness
-                        ),
-                      ),
-                      subtitle: Text(
-                        todo.id,
-                        style: const TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                      leading: Checkbox(
-                        fillColor: WidgetStateProperty.resolveWith<Color?>(
-                          (Set<WidgetState> states) {
-                            if (states.contains(WidgetState.selected)) {
-                              return Colors
-                                  .green; // Color when the checkbox is selected
-                            }
-                            return Colors.white; // Default color
-                          },
-                        ),
-                        value: todo.isCompleted,
-                        onChanged: (_) {
-                          todoList.toggleTodoStatus(todo.id);
-                        },
-                      ),
-                      trailing: IconButton(
-                        icon: const Icon(Icons.delete),
-                        color: Colors.amberAccent,
-                        onPressed: () {
+                  child: Slidable(
+                    startActionPane:
+                        ActionPane(motion: StretchMotion(), children: [
+                      SlidableAction(
+                        backgroundColor: Colors.red,
+                        borderRadius: BorderRadius.circular(16),
+                        onPressed: (context) {
                           todoList.removTodo(todo.id);
                         },
+                        icon: Icons.delete,
+                      )
+                    ]),
+                    child: Card(
+                      color: Colors.deepPurple[400],
+                      child: ListTile(
+                        title: Text(
+                          todo.title,
+                          style: TextStyle(
+                            color: Colors.white, // Text color
+                            fontWeight: FontWeight.bold, // Bold text
+                            decoration: todo.isCompleted
+                                ? TextDecoration
+                                    .lineThrough // Apply line-through if completed
+                                : TextDecoration
+                                    .none, // No decoration otherwise
+                            decorationColor: Colors.red, // Set the line color
+                            decorationThickness: 3.0, // Set the line thickness
+                          ),
+                        ),
+                        subtitle: Text(
+                          todo.id,
+                          style: const TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        leading: Checkbox(
+                          fillColor: WidgetStateProperty.resolveWith<Color?>(
+                            (Set<WidgetState> states) {
+                              if (states.contains(WidgetState.selected)) {
+                                return Colors
+                                    .green; // Color when the checkbox is selected
+                              }
+                              return Colors.white; // Default color
+                            },
+                          ),
+                          value: todo.isCompleted,
+                          onChanged: (_) {
+                            todoList.toggleTodoStatus(todo.id);
+                          },
+                        ),
+                        // trailing: IconButton(
+                        //   icon: const Icon(Icons.delete),
+                        //   color: Colors.amberAccent,
+                        //   onPressed: () {},
+                        // ),
                       ),
                     ),
                   ),
